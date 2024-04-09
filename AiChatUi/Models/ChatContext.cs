70f29e36752ef9a69e4711245f3f672b6c4e795f
Paul Trotter <ptrotter@axon.com>
2024-04-09 16:06:08 -0500

    Add conversation window and temperature

--- AiChatUi/Models/ChatContext.cs
+++ AiChatUi/Models/ChatContext.cs
@@ -7,10 +7,11 @@ public class ChatContext
     public Guid ChatId { get; set; } = Guid.NewGuid();
     public string ChatTitle { get; set; } = "New Chat";
     public string SystemInput { get; set; } = SystemPrompts.Claude3;
-    public double Temperature { get; set; }
+    public double Temperature { get; set; } = 0.7;
     public string TextInput { get; set; } = string.Empty;
     public List<Message> ChatMessages { get; set; } = new();
     public string SelectedModel { get; set; } = string.Empty;
     public DateTimeOffset LastMessageTime { get; set; } = DateTimeOffset.Now;
     public string TextAreaHeight { get; set; } = "4rem";
+    public int MaxMessages { get; set; } = 10;
 }
